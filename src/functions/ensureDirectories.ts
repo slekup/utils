c1ef297cf43add089b7fd396eaa6a688649b89ee
slekup <74949101+slekup@users.noreply.github.com>
2023-06-09 21:52:23 +1200

    fix ensureDirectories

--- src/functions/ensureDirectories.ts
+++ src/functions/ensureDirectories.ts
@@ -1,15 +1,10 @@
 import * as fs from 'fs';
 
-const directories = [
-  ['./static', './tmp'],
-  ['./static/v1/diep', './tmp/v1/diep'],
-  ['./static/v1/diep/tanks', './static/v1/diep/stats'],
-];
-
 /**
  * Ensure that all directories exist.
+ * @param directories The directories to ensure.
  */
-const ensureDirectories = async (): Promise<void> => {
+const ensureDirectories = async (directories: string[][]): Promise<void> => {
   for (const level of directories) {
     for (const dir of level) {
       await fs.promises.mkdir(dir, { recursive: true });
