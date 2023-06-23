e191e2a77901b05260a9af65ea733d249dbec5c4
slekup <74949101+slekup@users.noreply.github.com>
2023-06-23 22:43:48 +1200

    remove dep

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,4 +3,5 @@ export { default as ensureDirectories } from './ensureDirectories';
 export { default as ordinalNumber } from './ordinalNumber';
 export { default as splitArray } from './splitArray';
 export { default as stripIndents } from './stripIndents';
+export { default as uuidv4 } from './uuidv4';
 export { default as verifyEnvironment } from './verifyEnvironment';
