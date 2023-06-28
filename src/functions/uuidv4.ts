a98049e2d390f27f951ccf240125f1a63a65093a
slekup <74949101+slekup@users.noreply.github.com>
2023-06-28 22:45:50 +1200

    update uuid function

--- src/functions/uuidv4.ts
+++ src/functions/uuidv4.ts
@@ -1,16 +1,29 @@
+import crypto from 'crypto';
+
+/* eslint-disable no-bitwise */
+
 /**
  * Generate a random UUIDv4 string.
  * @returns A random UUIDv4 string.
  */
 const generateUUIDv4 = (): string => {
-  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-    /[xy]/g,
-    (character) => {
-      const randomNum = Math.random() * 16 || 0;
-      const value = character === 'x' ? randomNum : randomNum && 0x3;
-      return value.toString(16);
+  const randomBytes = crypto.randomBytes(16);
+
+  // Set the version and variant bits
+  randomBytes[6] = (randomBytes[6] ?? 0 & 0x0f) | 0x40; // Version 4
+  randomBytes[8] = (randomBytes[8] ?? 0 & 0x3f) | 0x80; // Variant 1*
+
+  const hexDigits = '0123456789abcdef';
+  let uuid = '';
+  for (let index = 0; index < 16; index += 1) {
+    const useIndex = randomBytes[index] ?? 0;
+    uuid += hexDigits[useIndex >> 4];
+    uuid += hexDigits[useIndex & 0x0f];
+    if (index === 3 || index === 5 || index === 7 || index === 9) {
+      uuid += '-';
     }
-  );
+  }
+
   return uuid;
 };
 
